e80285c874b4b401ab53b6ec3633f9f038b8af4c
Ramanan <ramanan.murugesan@gmail.com>
2025-05-24 17:44:10 +0200

    ex-2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,15 +3,20 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({ name: newName }))
   }
 
   const handleNameChange = (event) => {
+
+    if (persons.some(person => person.name === event.target.value)) {
+      alert(`${event.target.value} is already added to phonebook`)
+    }
+
     setNewName(event.target.value)
   }
 
@@ -20,7 +25,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
