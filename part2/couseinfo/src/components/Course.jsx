32342c3e8c33d7ffc8612be40439496b7c31bb98
Ramanan <ramanan.murugesan@gmail.com>
2025-05-24 17:07:59 +0200

    ex-2.5

---
+++ part2/couseinfo/src/components/Course.jsx
@@ -0,0 +1,34 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => {
+  return (<div>
+    {props.parts.map(part => <Part key={part.id} part={part} />)}
+  </div>)
+}
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <>
+      <p>Total of {total} exercises</p>
+    </>
+  )
+}
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total parts={props.course.parts} />
+    </div>
+  )
+}
+
+export default Course
