1d822185ff0aaae36c51815bfd73d8cc11dd0bf8
Ramanan <ramanan.murugesan@gmail.com>
2025-05-28 22:03:32 +0200

    ex-3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id == id)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
 app.get('/api/info', (request, response) => {
   const now = new Date();
   const response_string = `Phonebook has info for ${persons.length} people\n${now}`;
