d7c3fcdefd66b7154fc8fc323eef351771e01be3
Ramanan <ramanan.murugesan@gmail.com>
2025-05-31 22:09:34 +0200

    ex-4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    const likesSum = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    return likesSum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
