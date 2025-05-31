fa1f16f25a284e278fa7ce9e0789fd3927328038
Ramanan <ramanan.murugesan@gmail.com>
2025-05-31 22:20:30 +0200

    ex-4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,5 @@
 const dummy = (blogs) => {
-  return 1
+    return 1
 }
 
 const totalLikes = (blogs) => {
@@ -7,7 +7,18 @@ const totalLikes = (blogs) => {
     return likesSum
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    const mostLikedBlog = blogs.reduce((max, blog) => {
+        return blog.likes > max.likes ? blog : max;
+    }, blogs[0]);
+    return mostLikedBlog
+}
+
 module.exports = {
-  dummy,
-  totalLikes
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
