3a5cdbe8aa3c6b38536ec66787d4a8acba0ca130
Ramanan <ramanan.murugesan@gmail.com>
2025-06-14 22:38:48 +0200

    ex-5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -117,9 +117,11 @@ const App = () => {
 
       {user && (
         <>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
+          {[...blogs]
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
         </>
       )}
     </div>
