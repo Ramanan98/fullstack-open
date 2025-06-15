8bcb3327a128a05703734bf4bf2848e11a2c325e
Ramanan <ramanan.murugesan@gmail.com>
2025-06-15 22:03:05 +0200

    ex-5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,18 +1,19 @@
-import { render } from "@testing-library/react"
-import Blog from "./Blog"
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
 
 describe("Blog component", () => {
-  test("displaying a blog renders the blog's title and author, but does not render its URL or number of likes by default", () => {
-    const blog = {
-      title: "10 Tips to Improve Your JavaScript Skills",
-      author: "Jane Doe",
-      url: "https://devinsights.io/javascript-tips",
-      likes: 128,
-      user: {
-        username: "janedoe_dev",
-      },
-    }
+  const blog = {
+    title: "Test blog",
+    author: "Test author",
+    url: "https://test.com",
+    likes: 42,
+    user: {
+      username: "testuser",
+    },
+  }
 
+  test("displaying a blog renders the blog's title and author, but does not render its URL or number of likes by default", () => {
     const { container } = render(
       <Blog blog={blog} auth={true} onDelete={() => {}} />
     )
@@ -29,4 +30,40 @@ describe("Blog component", () => {
     let likes_div = container.querySelector(".likes")
     expect(likes_div).toBeNull()
   })
+
+  test("the blog's URL and number of likes are shown when the button controlling the shown details has been clicked", async () => {
+    const { container } = render(
+      <Blog blog={blog} auth={true} onDelete={() => {}} />
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url_div = container.querySelector(".url")
+    expect(url_div).toBeDefined()
+
+    const likes_div = container.querySelector(".likes")
+    expect(likes_div).toBeDefined()
+  })
+
+  test("if the like button is clicked twice, the event handler the component received as props is called twice", async () => {
+    const blogComponent = vi.fn()
+    const user = userEvent.setup()
+
+    render(
+        <Blog blog={blog} auth={true} onDelete={() => {}} />
+      )
+
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url_div = container.querySelector(".url")
+    expect(url_div).toBeDefined()
+
+    const likes_div = container.querySelector(".likes")
+    expect(likes_div).toBeDefined()
+  })
+
+
 })
