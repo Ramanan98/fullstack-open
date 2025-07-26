034e195a90a08543058dd40bbdef3386e8a9a2fb
Ramanan <ramanan.murugesan@gmail.com>
2025-07-26 14:59:14 +0530

    ex-8.9

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,32 +1,37 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
 
-  const books = []
+const Books = () => {
+  const { loading, error, data } = useQuery(ALL_BOOKS);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  const books = data?.allBooks || [];
 
   return (
     <div>
       <h2>books</h2>
-
       <table>
-        <tbody>
+        <thead>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+        </thead>
+        <tbody>
+          {books.map((book) => (
+            <tr key={`${book.title}-${book.author}`}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.published}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
