c046607ac09aec885133233d555e904f7faf9070
Ramanan <ramanan.murugesan@gmail.com>
2025-07-27 15:30:38 +0530

    ex-9.14

--- part9/patientor/backend/src/routes/patients.ts
+++ part9/patientor/backend/src/routes/patients.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import patientService from "../services/patientService";
 import { toNewPatient } from "../utils/patientParser";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -8,17 +9,19 @@ router.get("/", (_req, res) => {
   res.send(patientService.getNonSensitiveEntries());
 });
 
-router.post("/", (req, res) => {
+router.post("/", (req: Request, res: Response) => {
   try {
     const newPatient = toNewPatient(req.body);
     const addedPatient = patientService.addPatient(newPatient);
     res.json(addedPatient);
   } catch (error: unknown) {
-    let errorMessage = "Something went wrong.";
-    if (error instanceof Error) {
-      errorMessage += " Error: " + error.message;
+    if (error instanceof z.ZodError) {
+      res.status(400).json({ error: error.issues });
+    } else if (error instanceof Error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "An unknown error occurred" });
     }
-    res.status(400).send(errorMessage);
   }
 });
 
--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export interface Diagnosis {
   code: string;
   name: string;
@@ -22,3 +24,14 @@ export interface Patient {
 export type NonSensitivePatientEntry = Omit<Patient, "ssn">;
 
 export type NewPatient = Omit<Patient, "id">;
+
+const PatientSchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.string().date(),
+  ssn: z.string(),
+  gender: z.nativeEnum(Gender),
+  occupation: z.string(),
+});
+
+export const NewPatientSchema = PatientSchema;
+export type NewPatientRequest = z.infer<typeof NewPatientSchema>;
