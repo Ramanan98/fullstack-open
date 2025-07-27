ebd22c90ec6adb02da4eb1358afbb1b3bd24a337
Ramanan <ramanan.murugesan@gmail.com>
2025-07-27 17:59:46 +0530

    ex-9.18

--- part9/flight-diary/frontend/src/App.tsx
+++ part9/flight-diary/frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import type { DiaryEntry } from "./types";
-import { getAllDiaries } from "./services/diaryService";
+import { getAllDiaries, createDiary } from "./services/diaryService";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [newEntry, setNewEntry] = useState<Omit<DiaryEntry, "id">>({
+    date: "",
+    weather: "",
+    visibility: "",
+    comment: "",
+  });
 
   useEffect(() => {
     const fetchDiaries = async () => {
@@ -13,8 +19,69 @@ const App = () => {
     fetchDiaries();
   }, []);
 
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    try {
+      const addedEntry = await createDiary(newEntry);
+      setDiaries(diaries.concat(addedEntry));
+      setNewEntry({ date: "", weather: "", visibility: "", comment: "" });
+    } catch (error) {
+      console.error("Error adding diary entry:", error);
+    }
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = event.target;
+    setNewEntry({ ...newEntry, [name]: value });
+  };
+
   return (
     <div>
+      <h1>Add new entry</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          date:
+          <input
+            type="text"
+            name="date"
+            value={newEntry.date}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          visibility:
+          <input
+            type="text"
+            name="visibility"
+            value={newEntry.visibility}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          weather:
+          <input
+            type="text"
+            name="weather"
+            value={newEntry.weather}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          comment:
+          <input
+            type="text"
+            name="comment"
+            value={newEntry.comment}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+
       <h1>Flight Diary</h1>
       {diaries.map((diary) => (
         <div key={diary.id}>
--- part9/flight-diary/frontend/src/services/diaryService.ts
+++ part9/flight-diary/frontend/src/services/diaryService.ts
@@ -6,3 +6,16 @@ export const getAllDiaries = async (): Promise<DiaryEntry[]> => {
   const response = await fetch(baseUrl);
   return response.json();
 };
+
+export const createDiary = async (
+  newDiary: Omit<DiaryEntry, "id">
+): Promise<DiaryEntry> => {
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newDiary),
+  });
+  return response.json();
+};
